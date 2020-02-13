7537e5e3507250d56d84d4ab00e1d07d198b52f6
renyan11 <312144782@qq.com>
2020-02-13 17:14:25 +0800

    topic subscribers codes commit

--- yren-activemq-consumer/src/test/java/com/fh/yren/activemq/ActiveMqConsumerAplicationTest.java
+++ yren-activemq-consumer/src/test/java/com/fh/yren/activemq/ActiveMqConsumerAplicationTest.java
@@ -24,9 +24,11 @@ public class ActiveMqConsumerAplicationTest {
         //队列消费者们
         //aplicationTest.consumerQueueTest1();
         //aplicationTest.consumerQueueTest2();
-        //主题消费者们
+        //主题消费者们(非持久化)
         //aplicationTest.consumerTopicTest1();
-        aplicationTest.consumerTopicTest2();
+        //aplicationTest.consumerTopicTest2();
+        //主题消费者们(持久化)
+        aplicationTest.consumerTopicPersistent();
 
     }
 
@@ -138,6 +140,55 @@ public class ActiveMqConsumerAplicationTest {
                 }
             }));
             System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (consumer != null) {
+                try {
+                    consumer.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (session != null) {
+                try {
+                    session.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 主题消费者 --- 持久化
+     */
+    public void consumerTopicPersistent() {
+        try {
+            log.info("我是张三");
+            connection = connectFactory.createConnection();
+            //connection.start();
+            connection.setClientID("zhang-san");
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Topic topic2 = session.createTopic("ren-yan-topic-persistent");
+            //主题订阅者
+            TopicSubscriber subscriber = session.createDurableSubscriber(topic2, "remark");
+
+            connection.start();
+            Message message = subscriber.receive();
+            while(null != message){
+                TextMessage textMessage = (TextMessage)message;
+                log.info("收到的持久化主题消息为 = " + textMessage.getText());
+                //一直监听
+                message = subscriber.receive();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
--- yren-activemq-provider/src/test/java/com/fh/yren/activemq/ActiveMqProviderAplicationTest.java
+++ yren-activemq-provider/src/test/java/com/fh/yren/activemq/ActiveMqProviderAplicationTest.java
@@ -16,8 +16,10 @@ public class ActiveMqProviderAplicationTest {
         ActiveMqProviderAplicationTest aplicationTest = new ActiveMqProviderAplicationTest();
         //队列消息生产
         //aplicationTest.QueueProvider();
-        //主题消息生产
-        aplicationTest.TopicProvider();
+        //主题消息生产(非持久)
+        //aplicationTest.TopicProvider();
+        //主题消息生产(持久化)
+        aplicationTest.TopicProviderPersistent();
     }
 
     /**
@@ -34,6 +36,10 @@ public class ActiveMqProviderAplicationTest {
             session = connection.createSession(true, Session.SESSION_TRANSACTED);
             Queue queue = session.createQueue("ren-yan-queue-test");
             producer = session.createProducer(queue);
+            //持久化的消息
+            producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+            //非持久化的消息
+            //producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
             for (int i = 0; i < 5; i++) {
                 TextMessage textMessage = session.createTextMessage("test队列消息" + i);
                 producer.send(textMessage);
@@ -67,7 +73,7 @@ public class ActiveMqProviderAplicationTest {
     }
 
     /**
-     * Topic Provider 主题 生产者
+     * Topic Provider 主题 生产者 --- 非持久化
      */
     public void TopicProvider(){
         ConnectionFactory connectFactory = new ActiveMQConnectionFactory("tcp://101.133.232.101:61616");
@@ -80,8 +86,58 @@ public class ActiveMqProviderAplicationTest {
             session = connection.createSession(true, Session.SESSION_TRANSACTED);
             Topic topic = session.createTopic("ren-yan-topic-test");
             producer = session.createProducer(topic);
+            producer.setDeliveryMode(DeliveryMode.PERSISTENT);
             for (int i = 0; i < 5; i++) {
                 TextMessage textMessage = session.createTextMessage("test主题消息" + i);
+                producer.send(textMessage);
+            }
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (producer != null) {
+                try {
+                    producer.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (session != null) {
+                try {
+                    session.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Topic Provider 主题 生产者 --- 持久化
+     */
+    public void TopicProviderPersistent() {
+        ConnectionFactory connectFactory = new ActiveMQConnectionFactory("tcp://101.133.232.101:61616");
+        Connection connection = null;
+        Session session = null;
+        MessageProducer producer = null;
+        try {
+            connection = connectFactory.createConnection();
+            //connection.start();
+            session = connection.createSession(true, Session.SESSION_TRANSACTED);
+            Topic topic = session.createTopic("ren-yan-topic-persistent");
+            producer = session.createProducer(topic);
+            producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+            //启动位置
+            connection.start();
+            for (int i = 0; i < 5; i++) {
+                TextMessage textMessage = session.createTextMessage("topic的持久化主题消息---再生产数据---" + i);
                 producer.send(textMessage);
             }
             session.commit();
